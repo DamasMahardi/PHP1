c03f6bcb2de380a1b7d29ea2fca87f9ced81ed72
Your Name <you@example.com>
2023-05-19 16:43:27 +0700

    fix output akhir

--- index.php
+++ index.php
@@ -1,35 +1,26 @@
 <?php
-
 $data = <<<'EOD'
-X,-9\\\10\100\-5\\\0\\\\,A
-Y,\\13\\1\, B
-Z,\\\5\\\-3\\2\\\800,C
+X, -9\\\10\100\-5\\\0\\\\, A
+Y, \\13\\1\, B
+Z, \\\5\\\-3\\2\\\800, C
 EOD;
-
-$lines = explode("\n", $data);
-$result = [];
-foreach ($lines as $line) {
-    $parts = explode(',', $line);
-    $letter1 = $parts[0];
-    $letter2 = trim($parts[2]);
-    $numbers = preg_split('/\\\\+/', $parts[1]);
-    foreach ($numbers as $number) {
-        if (is_numeric($number)) {
-            $result[] = [$letter1, (int)$number, $letter2];
-        }
+$rows = explode("\n", $data);
+$results = [];
+foreach ($rows as $row) {
+    $cols = array_map('trim', explode(',', trim($row)));
+    $values = array_filter(explode('\\', $cols[1]), 'strlen');
+    foreach ($values as $value) {
+        $results[] = [
+            'var_xyz' => $cols[0],
+            'value' => $value,
+            'var_abc' => $cols[2]
+        ];
     }
 }
-usort($result, function ($a, $b) {
-    return $a[1] <=> $b[1];
-});
-$counter = [];
-foreach ($result as &$row) {
-    if (!isset($counter[$row[0]])) {
-        $counter[$row[0]] = 0;
-    }
-    $counter[$row[0]]++;
-    $row[] = $counter[$row[0]];
-}
-foreach ($result as $row) {
-    echo implode(', ', $row) . "\n";
+array_multisort(array_column($results, 'value'), SORT_ASC, $results);
+$count = [];
+foreach ($results as $result) {
+    $current = $result['var_abc'];
+    $count[$current] = ($count[$current] ?? 0) + 1;
+    echo ($result['var_xyz'] . ', ' . $result['value'] . ', ' . $result['var_abc'] . ', ' . $count[$current] . "\n");
 }
